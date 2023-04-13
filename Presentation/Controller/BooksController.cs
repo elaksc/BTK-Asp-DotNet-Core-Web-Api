0a08b3a11aaa9d628bcc952a71503684db0191b5
elaksc <elakascioglu3425@gmail.com>
2023-04-13 23:03:26 +0300

    Presantation has been edit

--- Presentation/Controller/BooksController.cs
+++ Presentation/Controller/BooksController.cs
@@ -1,6 +1,7 @@
 ﻿using Entities.DataTransferObjects;
 using Entities.Exceptions;
 using Entities.Models;
+using Entities.RequestFeatures;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Presentation.ActionFilters;
@@ -27,9 +28,9 @@ namespace Presentation.Controller
 
 
         [HttpGet]
-        public async Task<IActionResult> GetAllBooksAsync()
+        public async Task<IActionResult> GetAllBooksAsync([FromQuery]BookParameters bookParameters) // FromQuery kullandığımız zaman biliyoruz ki bu ifadeler bir query string üzerinden gelicek.
         {
-            var books = await _manager.BookService.GetAllBooksAsync(false);
+            var books = await _manager.BookService.GetAllBooksAsync(bookParameters, false);
             return Ok(books);
         }
 
