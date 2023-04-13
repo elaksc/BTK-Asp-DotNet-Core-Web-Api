96c24fbfdd21d149b7c79416c017dc8d4bb2343c
elaksc <elakascioglu3425@gmail.com>
2023-04-13 23:18:26 +0300

    MetaData class has been added

---
+++ Entities/RequestFeatures/MetaData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.RequestFeatures
+{
+    public class MetaData
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+
+        public bool HasPrevious => CurrentPage > 1; // Öncesinde sayfa var mı ?
+
+        public bool HasPage => CurrentPage < TotalPage; //  Sonrasında sayfa var mı?
+    }
+}
