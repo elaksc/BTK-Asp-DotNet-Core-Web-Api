2bcce0e62d7cc6e2646cb72cd42cc9f55c86bc9f
elaksc <elakascioglu3425@gmail.com>
2023-04-20 02:25:08 +0300

    OrderQueryBuilder

--- Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
+++ Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
@@ -34,21 +34,7 @@ namespace Repositories.EFCore.Extensions
             {
                 return books.OrderBy(b => b.Id);
             }
-            var orderParams = orderByQueryString.Trim().Split(',');
-            var propertyInfos = typeof(Book).GetProperties(BindingFlags.Public | BindingFlags.Instance);
-            var orderQueryBuilder = new StringBuilder();
-            foreach (var param in orderParams)
-            {
-                if (string.IsNullOrWhiteSpace(param)) continue;
-                var propertyFromQueryName = param.Split(' ')[0];
-                var objectProperty = propertyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
-
-                if (objectProperty is null) continue;
-                var direction = param.EndsWith(" desc") ? "descending" : "ascending";
-                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction},");
-
-            }
-            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+            var orderQuery = OrderQueryBuilder.CreateOrderQuery<Book>(orderByQueryString);
             if (orderQuery is null) return books.OrderBy(b => b.Id);
             return books.OrderBy(orderQuery);
         }
