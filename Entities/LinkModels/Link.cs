05dd4a05ef3cf72acacdf9e72911d2be96282f50
elaksc <elakascioglu3425@gmail.com>
2023-04-22 00:45:48 +0300

    LinkResponse

--- Entities/LinkModels/Link.cs
+++ Entities/LinkModels/Link.cs
@@ -25,13 +25,4 @@ namespace Entities.LinkModels
 
        
     }
-
-    public class LinkResourceBase
-    {
-        public LinkResourceBase()
-        {
-            
-        }
-        public List<Link> Links { get; set; } = new List<Link>();
-    }
 }
