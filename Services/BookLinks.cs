8bacf94d91d3b71fd4bcd10e05be777dafad9350
elaksc <elakascioglu3425@gmail.com>
2023-04-24 21:13:07 +0300

    BookLinks

---
+++ Services/BookLinks.cs
@@ -0,0 +1,82 @@
+﻿using Entities.DataTransferObjects;
+using Entities.LinkModels;
+using Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Net.Http.Headers;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class BookLinks : IBookLinks
+    {
+        private readonly LinkGenerator _linkGenerator;
+        private readonly IDataShaper<BookDto> _dataShaper;
+
+        public BookLinks(LinkGenerator linkGenerator, IDataShaper<BookDto> dataShaper)
+        {
+            _linkGenerator = linkGenerator;
+            _dataShaper = dataShaper;
+        }
+
+        public LinkResponse TryGenerateLinks(IEnumerable<BookDto> booksDto, string fields, HttpContext httpContext)
+            //BooksDTO -> Shaped Data ve Linked Data 
+        {
+            var shapedBooks = ShapeData(booksDto, fields);
+            if (ShouldGenerateLinks(httpContext))
+                return ReturnLinkedBooks(booksDto,fields,httpContext,shapedBooks);
+            return ReturnShaoedBooks(shapedBooks);
+
+        }
+
+        private LinkResponse ReturnLinkedBooks(IEnumerable<BookDto> booksDto, string fields, HttpContext httpContext, List<Entity> shapedBooks)
+        {
+            var bookDtoList = booksDto.ToList();
+            for(int i = 0; i <bookDtoList.Count; i++)
+            {
+                var bookLinks = CreateForBook(httpContext, bookDtoList[i], fields);
+                shapedBooks[i].Add("Links", bookLinks);
+            }
+            var bookCollection = new LinkCollectionWrapper<Entity>(shapedBooks);
+            return new LinkResponse { HasLinks = true, LinkedEntites = bookCollection };
+        }
+
+        private List<Link> CreateForBook(HttpContext httpContext, BookDto bookDto, string fields)
+        {
+            var links = new List<Link>()
+            {
+                new Link("a1","a2","a3"),
+                new Link("a1","a2","a3")
+
+            };
+            return links;
+
+        }
+
+        private LinkResponse ReturnShaoedBooks(List<Entity> shapedBooks)
+        {
+            return new LinkResponse() { ShapedEntities = shapedBooks };
+        }
+
+        private bool ShouldGenerateLinks(HttpContext httpContext)
+        {
+            var mediaType = (MediaTypeHeaderValue)httpContext.Items["AcceptHeaderMediaType"];
+                return mediaType.SubTypeWithoutSuffix
+                .EndsWith("haeoas", StringComparison.InvariantCultureIgnoreCase);
+
+            
+        }
+
+        private List<Entity> ShapeData(IEnumerable<BookDto> booksDto, string fields)
+        {
+            return _dataShaper.ShapeData(booksDto, fields)
+                .Select(b=>b.Entity)
+                .ToList();
+        }
+    }
+}
---
+++ Services/Contracts/IBookLinks.cs
@@ -0,0 +1,16 @@
+﻿using Entities.DataTransferObjects;
+using Entities.LinkModels;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Contracts
+{
+    public interface IBookLinks
+    {
+        LinkResponse TryGenerateLinks(IEnumerable<BookDto> booksDto, string fields, HttpContext httpContext);
+    }
+}
