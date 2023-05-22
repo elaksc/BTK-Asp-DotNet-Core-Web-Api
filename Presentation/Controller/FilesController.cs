6e68d8bfb5b24ebab50d3ecf63f9399adf7d535c
elaksc <elakascioglu3425@gmail.com>
2023-05-23 00:53:13 +0300

    File Upload

---
+++ Presentation/Controller/FilesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controller
+{
+    [ApiController]
+    [Route("api/files")]
+    public class FilesController : ControllerBase
+    {
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (!ModelState.IsValid) 
+            { 
+                return BadRequest();
+            }
+            var folder = Path.Combine(Directory.GetCurrentDirectory(), "Media");
+            if(!Directory.Exists(folder))
+                Directory.CreateDirectory(folder);
+            var path = Path.Combine(folder, file.FileName);
+
+            using(var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return Ok(new
+            {
+                file = file.FileName,
+                path = path,
+                size = file.Length
+            });
+        }
+    }
+}
